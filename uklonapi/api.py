b70fdc8eb475e96a2aef58dbf260c0ccd6f34b81
Mystic-Mirage <mm@m10e.net>
2024-09-27 17:00:05 +0300

    Decorator direct usage

--- uklonapi/api.py
+++ uklonapi/api.py
@@ -1,6 +1,7 @@
 from enum import StrEnum
 from functools import wraps
-from inspect import isgenerator, isgeneratorfunction
+from inspect import isgenerator, isgeneratorfunction, ismethod
+from types import MethodType
 
 from pydantic import TypeAdapter
 from requests import Response, Session
@@ -21,33 +22,46 @@ class APIVersion(StrEnum):
     V2 = "v2"
 
 
-def uklon_api(
-    method: APIMethod = APIMethod.GET, version: APIVersion = APIVersion.V1, json=True
+def _uklon_api_wrapper(
+    f: MethodType, method: APIMethod, version: APIVersion, json: bool
 ):
+    path = f.__name__.lstrip("_").replace("__", "/").replace("_", "-")
+    return_type = f.__annotations__.get("return")
+    type_adapter = TypeAdapter(return_type) if return_type else None
+
+    @wraps(f)
+    def wrapper(self: "UklonAPI", *args, **kwargs):
+        gen = (
+            f(self, *args, **kwargs)
+            if isgeneratorfunction(f)
+            else iter([f(self, **kwargs)])
+        )
+
+        kwargs = next(gen) or kwargs
+        kw = {"json" if json else "data": kwargs} if kwargs else {}
+        response = getattr(self, method)(version, path, **kw)
+
+        if isgenerator(gen):
+            gen.send(response)
+
+        if type_adapter:
+            return type_adapter.validate_json(response.text)
+
+    return wrapper
+
+
+def uklon_api(
+    method: APIMethod = APIMethod.GET,
+    version: APIVersion = APIVersion.V1,
+    *,
+    json: bool = True,
+):
+    if ismethod(method):
+        f, method = method, APIMethod.GET
+        return _uklon_api_wrapper(f, method, version, json)
+
     def decorator(f):
-        path = f.__name__.lstrip("_").replace("__", "/").replace("_", "-")
-        return_type = f.__annotations__.get("return")
-        type_adapter = TypeAdapter(return_type) if return_type else None
-
-        @wraps(f)
-        def wrapper(self, *args, **kwargs):
-            gen = (
-                f(self, *args, **kwargs)
-                if isgeneratorfunction(f)
-                else iter([f(self, **kwargs)])
-            )
-
-            kwargs = next(gen) or kwargs
-            kw = {"json" if json else "data": kwargs} if kwargs else {}
-            response = getattr(self, method)(version, path, **kw)
-
-            if isgenerator(gen):
-                gen.send(response)
-
-            if type_adapter:
-                return type_adapter.validate_json(response.text)
-
-        return wrapper
+        return _uklon_api_wrapper(f, method, version, json)
 
     return decorator
 
@@ -90,7 +104,7 @@ class UklonAPI:
         response.raise_for_status()
         return response
 
-    @uklon_api(APIMethod.POST, APIVersion.V1, json=False)
+    @uklon_api(APIMethod.POST, json=False)
     def _account__auth(self, grant_type, **kwargs):
         data = {
             "grant_type": grant_type,
@@ -109,11 +123,11 @@ class UklonAPI:
     def account_auth_refresh_token(self):
         self._account__auth("refresh_token", refresh_token=self.auth.refresh_token)
 
-    @uklon_api()
+    @uklon_api
     def favorite_addresses(self) -> list[Address]:
         pass
 
-    @uklon_api()
+    @uklon_api
     def me(self) -> Me:
         pass
 
