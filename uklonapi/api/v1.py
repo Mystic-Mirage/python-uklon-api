12e8f1d060f6797b379cc25f54d6129b4d169775
Mystic-Mirage <mm@m10e.net>
2024-09-27 04:32:17 +0300

    Add token refreshing support

--- uklonapi/api/v1.py
+++ uklonapi/api/v1.py
@@ -7,15 +7,20 @@ from ..types.me import Me
 class UklonAPIv1(UklonAPIBase):
     _base_url = f"{UklonAPIBase._base_url}/v1"
 
-    def account_auth(self, username: str, password: str):
+    def _account_auth(self, grant_type, **kwargs):
         data = {
-            "username": username,
-            "password": password,
-            "grant_type": "password",
+            "grant_type": grant_type,
             "client_id": self.client_id,
             "client_secret": self.client_secret,
+            **kwargs,
         }
         self.auth = Auth.model_validate_json(self._post("account/auth", data).text)
+
+    def account_auth_password(self, username: str, password: str):
+        self._account_auth("password", username=username, password=password)
+
+    def account_auth_refresh_token(self):
+        self._account_auth("refresh_token", refresh_token=self.auth.refresh_token)
 
     @validate
     def favorite_addresses(self) -> list[Address]:
