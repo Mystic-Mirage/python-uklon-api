7507f5618acd836750ac13c4a3a6d7a4320472f2
Mystic-Mirage <mm@m10e.net>
2024-10-08 22:59:34 +0300

    Update payment methods model

--- uklonapi/types/payment_methods.py
+++ uklonapi/types/payment_methods.py
@@ -5,8 +5,18 @@ class PaymentMethod(BaseModel):
     id: str
     description: str
     payment_type: str
-    card_type: str = None
+
+
+class PaymentMethodCard(PaymentMethod):
+    card_type: str
+
+
+class DefaultPaymentMethod(BaseModel):
+    id: str
+    payment_type: str
+    description: str
 
 
 class PaymentMethods(BaseModel):
-    payment_methods: list[PaymentMethod]
+    payment_methods: list[PaymentMethod | PaymentMethodCard]
+    default_payment_method: DefaultPaymentMethod
