27c45469a763f5e7dbfa58140fd76a55bad47512
Mystic-Mirage <mm@m10e.net>
2024-10-09 03:01:03 +0300

    DefaultPaymentMethod is a subclass of PaymentMethod now

--- uklonapi/types/payment_methods.py
+++ uklonapi/types/payment_methods.py
@@ -11,10 +11,8 @@ class PaymentMethodCard(PaymentMethod):
     card_type: str
 
 
-class DefaultPaymentMethod(BaseModel):
-    id: str
-    payment_type: str
-    description: str
+class DefaultPaymentMethod(PaymentMethod):
+    pass
 
 
 class PaymentMethods(BaseModel):
